47c1722ec1e45af2d203a084f6e58cb1fd9a06d3
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-25 05:49:06 +0000

    config done

--- javascriptCode/first.js
+++ javascriptCode/first.js
@@ -1,13 +1,15 @@
 console.log("HELLO Rudra");
-
-
 console.log("My name is rudra vaja");
-
 const UserID = 124578;
 //const can't be change
 
 let AccountName = "Rudra";
 //let can be change when runtime or inline
+
+
+CityName = "Bhavnagar";
+let emailid;
+
 
 
 
@@ -19,7 +21,7 @@ console.log(AccountName);
 
 
 
-console.table([UserID,AccountName,AccountPassword,AccountName]);//show output in table form with multiple variable
+console.table([UserID,AccountName,AccountPassword,AccountName,CityName,emailid]);//show output in table form with multiple variable
 
 
 
