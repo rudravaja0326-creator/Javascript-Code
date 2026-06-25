646039b755ad74d7b9f09243793fbe1bcd3fd1ea
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-25 10:08:37 +0000

    config done

--- javascriptCode/DataTypeConvertion.js
+++ javascriptCode/DataTypeConvertion.js
@@ -1,22 +1,43 @@
 let NumberAge = "12";
 let Name = "Rudra";
-let booean= true;
-let Convertion = Number(Name);//string to number
+let booean = true;
+let Convertion = Number(Name); //string to number
 
-let con2 = String(NumberAge);//string number to string
-let bool = Number(NumberAge);//string number to number
+let con2 = String(NumberAge); //string number to string
+let bool = Number(NumberAge); //string number to number
 //console.log(Convertion);//Output NaN
 
 //console.log(con2);//Output 12dhfha
-console.log(bool,typeof bool);//output 12 Number
-
+// console.log(bool,typeof bool);//output 12 Number
 
 let str = String(Name);
 
 // console.log(str);
 // console.log(typeof str);
 
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("5" - 2);
+// console.log(8 / 3);
+// console.log(8 % 3);
+// console.log((2 * 5 + 9) / (5 + 4));
 
+// console.log(true);
 
+let post = 5;
+let example = post++;
 
+console.log(post, "Postfix Example", example);
 
+let pre = 2;
+console.log(pre);
+let PreExample = ++pre;
+
+console.log(pre, "This is Prefix example", PreExample);
+
+console.log("2" == 2);//true
+console.log(2 ==2);//true
+console.log("2" === "2");//true
+console.log(2 === 2);//true
+console.log(2 ==="2");//flase
