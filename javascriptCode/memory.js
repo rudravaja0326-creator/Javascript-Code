c9f9bea2032e940f7b48fcbe59f167a03b492e7a
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-25 13:03:53 +0000

    config done

---
+++ javascriptCode/memory.js
@@ -0,0 +1,26 @@
+//Javascript have 2 types of memory
+
+
+// Primitive :-> NumBer(Int) , Bigint , string , null , boolean , Symbol
+//Non primitive :->  Array , Object , function
+
+// i) Stack (Premitive) ii)Heap (Primitive)
+
+
+var one = "Rudra";
+console.log("My name is",one);
+
+var two = "Vaja";
+console.log(two);
+
+var child = two;
+console.log(child);
+
+two = "JS";
+console.log(two);
+
+undefinedvar = 45;
+
+console.log(undefinedvar);
+
+
