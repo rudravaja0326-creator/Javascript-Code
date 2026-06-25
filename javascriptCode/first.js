1bb6965cf78328777cb7b6b48e36a5a4b8876293
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-25 05:37:45 +0000

    container config done

--- javascriptCode/first.js
+++ javascriptCode/first.js
@@ -1,7 +1,33 @@
 console.log("HELLO Rudra");
 
 
-comsole.log("My name is rudra vaja");
+console.log("My name is rudra vaja");
+
+const UserID = 124578;
+//const can't be change
+
+let AccountName = "Rudra";
+//let can be change when runtime or inline
+
+
+
+var AccountPassword = 4165487;
+//var change but its not recognize scop block
+
+console.log(UserID);
+console.log(AccountName);
+
+
+
+console.table([UserID,AccountName,AccountPassword,AccountName]);//show output in table form with multiple variable
+
+
+
+
+
+
+
+
 
 
 
