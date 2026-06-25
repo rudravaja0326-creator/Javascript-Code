fd9549964a3b1689a9221b528a75957702d7bc60
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-25 07:00:29 +0000

    config done

---
+++ javascriptCode/DataType.js
@@ -0,0 +1,23 @@
+let Age = 18; //Number
+const MobileNo = 9874561230; //Const Number
+let Name = "Rudra"; //String
+let Value = null; //Null
+let gmailid;
+//DataType in js
+
+// Number
+// Bigint
+// String
+// null
+// Object
+// Undefined
+// Boolean
+// Symbol
+
+console.table([
+  typeof Value,
+  typeof Name,
+  typeof Age,
+  typeof MobileNo,
+  typeof gmailid,
+]); //using typeof keyword we can find a variable type
---
+++ javascriptCode/DataTypeConvertion.js
@@ -0,0 +1,22 @@
+let NumberAge = "12";
+let Name = "Rudra";
+let booean= true;
+let Convertion = Number(Name);//string to number
+
+let con2 = String(NumberAge);//string number to string
+let bool = Number(NumberAge);//string number to number
+//console.log(Convertion);//Output NaN
+
+//console.log(con2);//Output 12dhfha
+console.log(bool,typeof bool);//output 12 Number
+
+
+let str = String(Name);
+
+// console.log(str);
+// console.log(typeof str);
+
+
+
+
+
