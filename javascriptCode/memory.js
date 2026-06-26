ca18c917502af261d3ffc01b1369028f3eaabfcb
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-26 04:52:48 +0000

    numbers and maths config done

--- javascriptCode/memory.js
+++ javascriptCode/memory.js
@@ -1,11 +1,9 @@
 //Javascript have 2 types of memory
 
-
 // Primitive :-> NumBer(Int) , Bigint , string , null , boolean , Symbol
 //Non primitive :->  Array , Object , function
 
 // i) Stack (Premitive) ii)Heap (Primitive)
-
 
 var one = "Rudra";
 console.log("My name is",one);
---
+++ javascriptCode/numbersandmaths.js
@@ -0,0 +1,21 @@
+var exampleofNumber = Number(200);//Output 200
+
+console.log(exampleofNumber);
+
+var exampleofNumber = new Number(200);//Output [Number 200]
+
+console.log(exampleofNumber);
+
+console.log(exampleofNumber.toString());//Convert into string
+
+console.log(exampleofNumber.toString().length);//count length of string
+
+console.log(exampleofNumber.toFixed(2));
+
+const perosicion = 1478.4545;
+
+console.log(perosicion.toPrecision(5));//Take an roundfigure value given number of count
+
+
+
+
