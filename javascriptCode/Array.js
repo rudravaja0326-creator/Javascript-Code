0e53a6e4c435437d2dcac1cf232fe79c01c160b2
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-29 11:04:01 +0000

    Arrays config done

---
+++ javascriptCode/Array.js
@@ -0,0 +1,49 @@
+const Arry = [0,1,2,3,4];//An array is a special object that stores multiple values in a single variable using index numbers starting from 0.
+
+// console.log(Array.length);//output 5
+// console.log(Arry[1]);//Output 1
+
+// const objectname = new Array(4);
+// objectname[6]="Rudra"//creates an array with a length of 4 and four empty slots, not four values.
+// console.log(objectname[6]);//Output [ <4 empty items> ]
+// console.log(objectname.length);//Output will be 4
+
+
+console.log(Arry.push(9));//Push Element but return updted length of array.
+console.log(Arry);
+
+
+Arry.pop()
+console.log(Arry);
+const Array = [10, 20, 30];
+
+console.log("10 is Here:",Array.includes(20));//return true or false, if that value have in that array
+console.log("index is:",Array.indexOf(20));//return index number to given element value.
+
+console.log(Array.pop());//ouput was 30 show that 30 was pop
+console.log(Array);//Output is [10,20]
+
+Arry.unshift(9);//add element in starting position
+
+console.log(Arry);
+
+Arry.shift();//remove element at starting position
+console.log(Arry);//output is [0,1,2,3,4]
+
+Arry.shift();
+console.log(Arry);
+
+const myArr = [0,1,2,3,4,5]
+
+console.log("A ", myArr); // A [0, 1, 2, 3, 4, 5]
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1); // [1, 2]
+console.log("B ", myArr); // B [0, 1, 2, 3, 4, 5]
+
+const myn2 = myArr.splice(1, 3)
+
+console.log("C ", myArr); // C [0, 4, 5]
+console.log(myn2); // [1, 2, 3]
+console.log(myArr); // [0, 4, 5]
---
+++ javascriptCode/SecondArrayTopic.js
@@ -0,0 +1,25 @@
+const marvel = ["captain america","thor","Hulk","Ironman"];
+
+const dc = ["Batman","Superman","Flash","Wonder Women"];
+
+
+// marvel.push(dc);//Add element and change original array
+
+// console.log(marvel);
+
+// console.log(marvel[4][1]);
+
+let allhero = marvel.concat(dc);//Join arrays and create a new array
+
+console.log(allhero);
+
+
+// let SpreadOprator = [...marvel,...dc];/The spread operator (...) expands an array, object, or iterable into individual elements.
+console.log("This is spread oprator",SpreadOprator);
+
+
+
+    
+
+
+
