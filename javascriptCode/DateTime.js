c164081d4e3d4e20931e34885f386600df982729
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-06-29 08:59:12 +0000

    DateTime config done

--- javascriptCode/DateTime.js
+++ javascriptCode/DateTime.js
@@ -1,17 +1,28 @@
 var date = new Date();
 
-console.log(date);//Type is object
+console.log(date); //Type is object
 
-console.log(typeof Date());//Type is string
-
+console.log(typeof Date()); //Type is string
 
 // if you define date without making object when their datatype was string ,
 // when using new keyword make object of date then that consider as object.
 
 console.log(Date());
 
-
 var datefuncTion = new Date("2026-06-26");
 console.log(datefuncTion.toLocaleString());
 
-console.log(String.fromCharCode("h"));
+let functionOfDate = new Date();
+console.log(functionOfDate.getDate());
+console.log(functionOfDate.getFullYear());
+console.log(functionOfDate.toString());
+console.log(functionOfDate.getTimezoneOffset());
+console.log(functionOfDate.getUTCDate());
+console.log(functionOfDate.getDate());
+console.log(functionOfDate.getMilliseconds());
+console.log(functionOfDate.toDateString());
+console.log(functionOfDate.toLocaleDateString());
+console.log(functionOfDate.toLocaleString());
+console.log(functionOfDate.toISOString());
+console.log(functionOfDate.getMonth() + 1);
+console.log(functionOfDate.getUTCMonth() + 1);
