a6785ce743c81cd6d2ccdf960a9c9c718001bf6d
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-07-01 09:32:15 +0000

    arrow config done

--- javascriptCode/Arrow_.js
+++ javascriptCode/Arrow_.js
@@ -2,8 +2,33 @@
 //         return n1+n2;
 // }//when use {} braces use return keyword.
 
+
+//An Arrow Function is a shorter and cleaner way to write functions in JavaScript.
+
+
 const arrow = (n1,n2)=>(n1+n2);
-arrow(2,5);//હોય તો
+arrow(2, 5);
+
+// const obj = {
+//   name: "Rudra",
+//   age: 19,
+//   meth: function (name1, age1) {
+//     console.log(`My name is ${name1} and my age is ${age1}`);
+//   },
+// };
+
+// obj.meth("Rudra", 19);
+
+const obj = {
+  name: "Rudra",
+  age: 19,
+  meth: () => {
+    console.log(`My name is ${this.name} and my age is ${this.age}`);
+  },
+};
+
+obj.meth();
+
 
 
 
