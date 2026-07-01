8c0d347d036b47de4072b02914cbae6e2e46df2b
rudravaja0326-creator <rudravaja0326@gmail.com>
2026-07-01 07:03:18 +0000

    Scops and arrow config done

---
+++ javascriptCode/Arrow_.js
@@ -0,0 +1,9 @@
+// const arrow = (n1,n2)=>{
+//         return n1+n2;
+// }//when use {} braces use return keyword.
+
+const arrow = (n1,n2)=>(n1+n2);
+arrow(2,5);//હોય તો
+
+
+
--- javascriptCode/Scops.js
+++ javascriptCode/Scops.js
@@ -1,15 +1,71 @@
-function globle(){
-        const userName = "Rudra";
-        console.log("Globle Scope Function:",userName);
-      function Child(){
-        const Local = "Inner";
-        console.log(userName);
-        console.log("Local Scope:",Local);
-      }
+function globle() {
+  const userName = "Rudra";
+  console.log("Globle Scope Function:", userName);
+  function Child() {
+    const Local = "Inner";
+    console.log(userName);
+    console.log("Local Scope:", Local);
+    console.log(Local);
+  }
 
-console.log("Globle Scope:",userName);
+  // console.log(Local);//cant be use property.
+  Child(); //you can use whole function itself.
+
+  console.log("Globle Scope:", userName);
 }
 
 globle();
 
+//==================================== Differ Functions Variablescop =======================//
+console.log(access(8));
+function access(n) {
+  return n + 1;
+}
 
+// console.log(local(5)); //you can not use that kind of syntax when hold function in variable.
+const local = function notaccess(n2) {
+  return n2 + 9;
+};
+
+// 1. Global Scope
+
+let name = "Rudra"; // Global variable
+
+function showName() {
+  console.log(name);
+}
+
+showName(); // Rudra
+console.log(name); // Rudra
+
+// 2. Function Scope
+
+function myFunction() {
+  var age = 20;
+  console.log(age);
+}
+
+myFunction(); // 20
+// console.log(age); // Error: age is not defined
+
+// 3. Block Scope
+{
+  let city = "Bhavnagar";
+  const country = "India";
+
+  console.log(city); // Bhavnagar
+  console.log(country); // India
+}
+
+// console.log(city); // Error
+// console.log(country); // Error
+
+// 4. Local Scope
+
+function test() {
+  let message = "Hello";
+  console.log(message);
+}
+
+test(); // Hello
+// console.log(message); // Error
