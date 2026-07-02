9861933935024b3fc50d50b769819f948eab7ab8
Rudra <rudravaja0326@gmail.com>
2026-07-02 10:46:34 +0000

    level up

---
+++ Control_Flow/iteration_Loops.js
@@ -0,0 +1,6 @@
+    for (let index = 0; index < 11; index++) {
+        console.log("This is index",index);
+        for(let j=0;j<11;j++){
+       console.log(index +'*'+j+ '='+index*j);
+        }
+    }
