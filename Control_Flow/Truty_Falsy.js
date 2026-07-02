0ba4b68f6601ac74a8b28171f6c617921acde301
Rudra <rudravaja0326@gmail.com>
2026-07-02 10:47:05 +0000

    truty_falsy config done

---
+++ Control_Flow/Truty_Falsy.js
@@ -0,0 +1,13 @@
+// Falsy Values in JavaScript
+
+// A falsy value is a value that JavaScript treats as false when used in a Boolean context (such as an if condition).
+
+// [ false, 0, -0, BigInt (0n), '', null, undefined, NaN ]
+
+
+// Truthy Values
+
+// Everything that is not falsy is generally truthy.
+
+// false, 0, -0, 0n, "", null, undefined, NaN , function(){}.
+
