1653583f41fc7e5a9d61b8c2bc08cc5d7a7895c8
Rudra <rudravaja0326@gmail.com>
2026-07-02 10:52:19 +0000

    done

--- Control_Flow/iteration_Loops.js
+++ Control_Flow/iteration_Loops.js
@@ -1,5 +1,7 @@
-    for (let index = 0; index < 11; index++) {
-        console.log("This is index",index);
+//print 1 to 10 table using for loop
+
+for (let index = 0; index < 11; index++) {
+         console.log("This is index",index);
         for(let j=0;j<11;j++){
        console.log(index +'*'+j+ '='+index*j);
         }
