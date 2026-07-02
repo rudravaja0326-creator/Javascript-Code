1f2f984b0f07afec98706f4c1bf54ada6da3fce6
Rudra <rudravaja0326@gmail.com>
2026-07-02 11:12:47 +0000

    while loop config done

--- Control_Flow Level 2/iteration_Loops.js
+++ Control_Flow Level 2/iteration_Loops.js
@@ -1,8 +1,22 @@
 //print 1 to 10 table using for loop
 
+// For loop
+
+
 for (let index = 0; index < 11; index++) {
          console.log("This is index",index);
         for(let j=0;j<11;j++){
        console.log(index +'*'+j+ '='+index*j);
         }
     }
+
+
+// While Loop
+
+
+
+
+
+
+
+
