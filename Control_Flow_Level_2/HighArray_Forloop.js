ead2666a652ba8d98b3f643b6c56165a47acb240
Rudra <rudravaja0326@gmail.com>
2026-07-03 09:35:28 +0000

    Filter donfig done

--- Control_Flow_Level_2/HighArray_Forloop.js
+++ Control_Flow_Level_2/HighArray_Forloop.js
@@ -1,112 +1,160 @@
-//The for...of loop is used to iterate over the values of an iterable object such as:
+// //The for...of loop is used to iterate over the values of an iterable object such as:
 
-// Arrays
-// Strings
-// Maps
-// Sets
+// // Arrays
+// // Strings
+// // Maps
+// // Sets
 
-// const Loop = "My name is rudra Vaja";
+// // const Loop = "My name is rudra Vaja";
 
-// for(const print of Loop){
-// console.log(print);
+// // for(const print of Loop){
+// // console.log(print);
+// // }
+
+
+// // const array = ["Hello","Insta","Whatsapp","Snap"];
+
+// // for(const num of array){
+// //     console.log(num);
+// // }
+
+
+
+// // let map = new Map();
+// // map.set("In","India");
+// // map.set("Af","Africa");
+// // map.set("Can","Caneda");
+
+// // console.log(map);
+
+
+// // for (const [key,value] of map) {
+// //    console.log(`Key :- ${key} Value :- ${value}`); 
+// // }
+// // //For of loop iterates an values , used on [Arrays, Strings, Maps, Sets].
+
+// // //////////////////////////////////// For in ///////////////////////////////////
+
+// // const obj = {
+// //     name:"Rudra",
+// //     surname:"Vaja",
+// //     course:"B.Tech",
+// //     CollegeName:"Gmiu",
+// //     id:1546789
+// // };
+
+
+// // for (const value in obj) {
+// //              console.log(value);
+// //              console.log(obj[value]);    
+// // }
+
+// // // The for...in loop is used to iterate over the keys (property names) of an object.
+
+
+
+// // const Array = ["Yo",1,2,3,4,6,7,9];
+
+// // Array.forEach( value=> {
+// //     console.log(value);
+// // });
+
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// function printitem(items, ind , arr){
+//     console.log(items, ind , arr);
+//     console.log(ind)
 // }
 
 
-// const array = ["Hello","Insta","Whatsapp","Snap"];
-
-// for(const num of array){
-//     console.log(num);
-// }
+// coding.forEach(printitem);
 
 
 
-// let map = new Map();
-// map.set("In","India");
-// map.set("Af","Africa");
-// map.set("Can","Caneda");
-
-// console.log(map);
+// // coding.forEach((items, ind , arr)=>{console.log(items,ind,arr)});
 
 
-// for (const [key,value] of map) {
-//    console.log(`Key :- ${key} Value :- ${value}`); 
-// }
-// //For of loop iterates an values , used on [Arrays, Strings, Maps, Sets].
-
-// //////////////////////////////////// For in ///////////////////////////////////
-
-// const obj = {
-//     name:"Rudra",
-//     surname:"Vaja",
-//     course:"B.Tech",
-//     CollegeName:"Gmiu",
-//     id:1546789
-// };
+// // {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{ For each loop }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+// // forEach() Method
+// // The forEach() method executes a function once
+// // for each element in an array.
+// // It is used to perform an action on every array item.
 
 
-// for (const value in obj) {
-//              console.log(value);
-//              console.log(obj[value]);    
-// }
+// const AB = [{
+//     nameOFStudent:"Rudra",
+//     id:45
+// },
+// {
+//     nameOFStudent:"sam",
+//     id:46
+// },
+// {
+//     nameOFStudent:"David",
+//     id:47
+// },
+// {
+//     nameOFStudent:"Parker",
+//     id:48
+// },
+// {
+//     nameOFStudent:"Peter",
+//     id:49
+// },
+// ];
 
-// // The for...in loop is used to iterate over the keys (property names) of an object.
+// console.log(AB);
 
-
-
-// const Array = ["Yo",1,2,3,4,6,7,9];
-
-// Array.forEach( value=> {
-//     console.log(value);
+// AB.forEach((itemsi)=>{
+//     console.log(itemsi.nameOFStudent , itemsi.id);
 // });
 
-
-const coding = ["js", "ruby", "java", "python", "cpp"]
-
-function printitem(items, ind , arr){
-    console.log(items, ind , arr);
-    console.log(ind)
-}
-
-
-coding.forEach(printitem);
-
-
-
-// coding.forEach((items, ind , arr)=>{console.log(items,ind,arr)});
-
-
-// {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{ For each loop }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
-// forEach() Method
-// The forEach() method executes a function once
-// for each element in an array.
-// It is used to perform an action on every array item.
-
-
-const AB = [{
-    nameOFStudent:"Rudra",
-    id:45
-},
-{
-    nameOFStudent:"sam",
-    id:46
-},
-{
-    nameOFStudent:"David",
-    id:47
-},
-{
-    nameOFStudent:"Parker",
-    id:48
-},
-{
-    nameOFStudent:"Peter",
-    id:49
-},
+const objarray = [
+  {
+    title: "JavaScript Basics",
+    genre: "Programming",
+    publish: 2023,
+    edition: 1
+  },
+  {
+    title: "JavaScript Basics", // Same title
+    genre: "Programming",       // Same genre
+    publish: 2024,
+    edition: 2
+  },
+  {
+    title: "Python Guide",
+    genre: "Programming",
+    publish: 2022,
+    edition: 3
+  },
+  {
+    title: "Marvel Comics",
+    genre: "Comics",
+    publish: 2021,
+    edition: 4
+  },
+  {
+    title: "Marvel Comics", // Same title
+    genre: "Comics",        // Same genre
+    publish: 2000,
+    edition: 5
+  }
 ];
 
-console.log(AB);
+console.log(objarray);
 
-AB.forEach((itemsi)=>{
-    console.log(itemsi.nameOFStudent , itemsi.id);
-});
+
+
+
+
+let same = objarray.filter((ti)=>{
+    if(ti.genre === "Programming"){
+    console.log(ti.genre);
+    }
+})
+
+same = objarray.filter(gi=>gi.publish >= 2022)
+console.log(same);
 
