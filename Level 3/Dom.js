539a03e7c7fffa2b8a4f6ee2c9f9e06cbd88e1de
Rudra <rudravaja0326@gmail.com>
2026-07-06 04:42:32 +0000

    Dom config Done

---
+++ Level 3/Dom.js
@@ -0,0 +1,6 @@
+// DOM (Document Object Model).
+
+
+
+// The DOM (Document Object Model) is a programming interface that represents an HTML or XML document as a tree of objects. It allows JavaScript to access, modify, add, or remove elements and content on a web page dynamically.
+
