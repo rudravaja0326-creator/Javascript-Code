2bf9ecf79928abb55f6e1252115c3be28210297f
Rudra <rudravaja0326@gmail.com>
2026-07-09 12:53:04 +0000

    Bubbking config done

---
+++ Level 3/practice.js
@@ -0,0 +1,20 @@
+// let x = 5;
+// let y = x++;
+
+// console.log(x);
+// console.log(y);
+let a = [1, 2, 3];
+
+let b = a;
+ 
+b.push(4);
+
+console.log(b);
+
+console.log(typeof null);
+
+
+
+
+
+
